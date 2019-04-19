82aa70b07a0127032048e780c7399601b7ac6aad
AlainMk <alainmukingiro.am@gmail.com>
2019-04-19 18:47:55 +0100

    init commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (0, 'Viande de boeuf à la sauce tomate', 'bougie qui fonctionne comme une ampoule mais sans électricité !', 'https://firebasestorage.googleapis.com/v0/b/food-star.appspot.com/o/Chinese-General-Tso-Chicken12.jpg?alt=media&token=7ff4ed85-9c6c-4196-b75c-756932cfcebd', 22.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (1, 'Double cheez burger avec des nachos', 'Chaise rare avec non pas 1 ni 2 mais 3 pieds', 'https://firebasestorage.googleapis.com/v0/b/food-star.appspot.com/o/burger.jpg?alt=media&token=e28e23c9-0ae6-400b-9655-54776f16a5f5', 95.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (2, 'Boulette avec du piment chaud', 'Ce cheval ne portera certainement pas blanche neige, mais sans problème les nains', 'https://firebasestorage.googleapis.com/v0/b/food-star.appspot.com/o/3b7cf5_8d984cfdbc6e4b9aa45838d812b2dd6e_mv2.jpg?alt=media&token=d291e74b-3893-4364-89b2-9a2a9ba08673', 360.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (3, 'Poulet maillot avec des sochie', 'Ne passe pas au four', 'https://firebasestorage.googleapis.com/v0/b/food-star.appspot.com/o/Chilli-Chicken.jpg?alt=media&token=0f04f0ed-b279-4bed-8db2-dda425afd4df', 620.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (4, 'Patte italienne avec du piment grillé', 'Vous donne droit à l''équivalent de 3/0 voeux', 'https://firebasestorage.googleapis.com/v0/b/food-star.appspot.com/o/5ad792ffbd967146008b45d9-750-562.jpg?alt=media&token=10180e56-5fe9-462a-bf55-df1a36db04b2', 1200.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (5, 'Cheez burger américaine mcDonnald', 'les minutes et même les secondes. Ne fait pas de café', 'https://firebasestorage.googleapis.com/v0/b/food-star.appspot.com/o/burger1920x960-1048x524.jpg?alt=media&token=5f7b79e8-6ca8-4d81-8ef2-85ebd551e9b2', 180.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (6, 'Hamberger KFC pro', 'Pour réaliser vos opérations chirugicales sur votre Hamster!', 'https://firebasestorage.googleapis.com/v0/b/food-star.appspot.com/o/863385284hamburger-fast-food-french-fries-Favim.com-483020.jpg?alt=media&token=dae0ed67-3885-4002-801a-9d2404d9422f', 210.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (7 , 'Treple cheez burger et des nachos', 'Risque de choc électrique', 'https://firebasestorage.googleapis.com/v0/b/food-star.appspot.com/o/burger-cropped.jpg?alt=media&token=5583936c-baf2-42b9-8076-17a13b94fffd', 730.0);
